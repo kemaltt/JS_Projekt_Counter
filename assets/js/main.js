59b0e459a69be0d00c2c9c8cfae7e6c729ac96a5
kemaltt <ttnckemal@gmail.com>
2022-04-20 15:58:26 +0200

    lösung3-adding

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,56 +43,90 @@
 // }
 
 //Lösung 2----------------------------
+// const output = document.getElementById('output')
+// let count = 0;
+// output.innerHTML = count
+
+// const plusOne = document.getElementById('plusOne')
+// const minusOne = document.getElementById('minusOne')
+// const plusTen = document.getElementById('plusTen')
+// const minusTen = document.getElementById('minusTen')
+// const plusHundred = document.getElementById('plusHundred')
+// const minusHundred = document.getElementById('minusHundred')
+// const multiplyTwo = document.getElementById('multiplyTwo')
+// const reset = document.getElementById('reset')
+
+// plusOne.addEventListener('click', () => {
+//     count++
+//     output.innerHTML = count
+// })
+// minusOne.addEventListener('click', () => {
+//     count--
+//     output.innerHTML = count
+// })
+
+// plusTen.addEventListener('click', () => {
+//     count += 10
+//     output.innerHTML = count
+// })
+// minusTen.addEventListener('click', () => {
+//     count -= 10
+//     output.innerHTML = count
+// })
+// plusHundred.addEventListener('click', () => {
+//     count += 100
+//     output.innerHTML = count
+// })
+// minusHundred.addEventListener('click', () => {
+//     count -= 100
+//     output.innerHTML = count
+// })
+
+// multiplyTwo.addEventListener('click', () => {
+//     count *= 2
+//     output.innerHTML = count
+// })
+// reset.addEventListener('click', () => {
+//     count = 0
+//     output.innerHTML = count
+// })
+
+//Lösung 3-----------------------
+
 const output = document.getElementById('output')
 let count = 0;
 output.innerHTML = count
 
-const plusOne = document.getElementById('plusOne')
-const minusOne = document.getElementById('minusOne')
-const plusTen = document.getElementById('plusTen')
-const minusTen = document.getElementById('minusTen')
-const plusHundred = document.getElementById('plusHundred')
-const minusHundred = document.getElementById('minusHundred')
-const multiplyTwo = document.getElementById('multiplyTwo')
-const reset = document.getElementById('reset')
+const inputCounter = document.querySelectorAll('.counter')
+console.log(inputCounter);
+for (let i = 0; i < inputCounter.length; i++) {
+    const input = inputCounter[i];
+    console.log(input);
 
-plusOne.addEventListener('click', () => {
-    count++
-    output.innerHTML = count
-})
-minusOne.addEventListener('click', () => {
-    count--
-    output.innerHTML = count
-})
+    input.addEventListener('click', () => {
+        console.log(input);
 
-plusTen.addEventListener('click', () => {
-    count += 10
-    output.innerHTML = count
-})
-minusTen.addEventListener('click', () => {
-    count -= 10
-    output.innerHTML = count
-})
-plusHundred.addEventListener('click', () => {
-    count += 100
-    output.innerHTML = count
-})
-minusHundred.addEventListener('click', () => {
-    count -= 100
-    output.innerHTML = count
-})
+        if (input.value == 'Reset') {
+            count = 0
+            output.innerHTML = count
+            console.log(output);
+        } else {
+            let newInput = input.value
+            let numInput = parseInt(newInput)
+            console.log(numInput);
+            count = count + numInput
+            output.innerHTML = count
+            console.log(output);
+        }
 
-multiplyTwo.addEventListener('click', () => {
-    count *= 2
-    output.innerHTML = count
-})
-reset.addEventListener('click', () => {
-    count = 0
-    output.innerHTML = count
-})
+        if (input.value == '*2') {
+            let newInput = input.value
+            let numInput = parseInt(newInput)
+            console.log(numInput);
+            count = count + numInput
+            output.innerHTML = count
+        }
 
-//Lösung 3-----------------------
+    })
 
-// const output = document.getElementById('output')
-// let count = 0;
-// output.innerHTML = count
+}
